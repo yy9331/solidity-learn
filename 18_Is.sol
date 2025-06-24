bdae7291584b4fbd7b0cd88d517584a853876b1b
yy9331 <yuyi.gz@163.com>
2025-06-24 16:03:19 +0800

    inheritance

--- 18_Is.sol
+++ 18_Is.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract A {
-    
+event ConstructorA(uint x);
+event ConstructorC(uint y);
+event ConstructorB();
+
+contract D {}
+
+contract A is D {
+    constructor(uint x) { emit ConstructorA(x); }
+    function foo () public  virtual pure returns (string memory) {
+        return "A contract";
+    }
+}
+contract C is D {
+    constructor(uint y) { emit ConstructorC(y); }
+    function foo () public  virtual pure returns (string memory) {
+        return "A contract";
+    }
+}
+
+contract B is A, C {
+    constructor() A(100) C(200) { emit ConstructorB(); }
+    function foo () public override(A, C) pure returns (string memory) {
+        return "B contract";
+    }
 }
